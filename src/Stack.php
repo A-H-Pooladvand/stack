d7e3a13ebe4db028094e2fc39b26bd55819047b2
Amirhossein Pooladvand <a.h.pooladvand@gmail.com>
2018-12-28 14:37:35 -0500

    Initial setup

---
+++ src/Stack.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Stack;
+
+class Stack
+{
+    /**
+     * @var array $stack
+     */
+    private $stack = [];
+
+    /**
+     * Push an item to the stack
+     *
+     * @param int|string|array|mixed $item
+     */
+    public function push($item): void
+    {
+        $this->stack[] = $item;
+    }
+
+    /**
+     * Pop last value from stack.
+     *
+     * @return mixed
+     */
+    public function pop()
+    {
+        return array_pop($this->stack);
+    }
+
+    /**
+     * Returns last value inside stack.
+     *
+     * @return mixed
+     */
+    public function peek()
+    {
+        $stackValues = array_values($this->stack);
+
+        return end($stackValues);
+    }
+
+    /**
+     * Validates whether stack is empty.
+     *
+     * @return bool
+     */
+    public function isEmpty(): bool
+    {
+        return empty($this->stack);
+    }
+
+    /**
+     * Get all of the values inside the stack.
+     *
+     * @return array
+     */
+    public function get(): array
+    {
+        return $this->stack;
+    }
+}
